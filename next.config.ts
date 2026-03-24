8a793fc7259b34a07fac85cfb0cadd283429c724
Dries Vanvlasselaer <dries@v-b.be>
2026-03-24 13:49:27 +0100

    Fix: enable static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // DEZE REGEL IS NODIG!
 };
 
 export default nextConfig;
